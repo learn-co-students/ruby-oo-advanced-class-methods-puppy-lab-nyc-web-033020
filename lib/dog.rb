1b068bab9ebf08f186cc0810f80ee447dd68cb0b
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-01 21:41:25 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+  @@all = []
+  @@names = []
+
+  def initialize(name)
+    @name = name
+    self.save
+    @@names << name
+    #save_name
+  end
+
+  #def save_name
+  #  @@names << @name
+  #end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@names.uniq.each { |dog| puts dog }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
